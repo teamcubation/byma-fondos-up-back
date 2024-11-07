1fd125ad6073b3bf4ad20bd37c3268c89bb6be8b
Tavo-94 <gustavo.mph@gmail.com>
2024-11-07 10:11:31 -0300

    Scaffolding inicial

---
+++ src/test/java/com/byma/fondos_up_back/FondosUpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byma.fondos_up_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondosUpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
