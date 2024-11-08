eaa071f55d415c0edcbd9817e210d3b691527971
MariaPaulaBaldivia <m.paulabaldivia00@gmail.com>
2024-11-08 11:53:53 -0300

    Agrego clase GlobalExceptionHandler y modifico atributos del ErrorMessageResponse

--- src/main/java/com/byma/fondos_up_back/exception_handler/ErrorMessageResponse.java
+++ src/main/java/com/byma/fondos_up_back/exception_handler/ErrorMessageResponse.java
@@ -1,19 +1,18 @@
 package com.byma.fondos_up_back.exception_handler;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data
 @Builder
 public class ErrorMessageResponse {
-    private String exception;
-    private String code;
-    private String message;
-    private String path;
+    private final String exception;
+    private final int status;
+    private final String message;
+    private final String path;
+    private final String method;
     @Builder.Default
     private final LocalDateTime timestamp = LocalDateTime.now();
 }
