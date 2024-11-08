be2e688c93ae57b776aa9c84fa2808aee817bc98
Lucia-azd <azd.lucia@gmail.com>
2024-11-08 12:01:59 -0300

    config de logging

--- src/main/java/com/byma/fondos_up_back/application/service/GerenteService.java
+++ src/main/java/com/byma/fondos_up_back/application/service/GerenteService.java
@@ -4,11 +4,13 @@ import com.byma.fondos_up_back.application.port.in.GerenteInPort;
 import com.byma.fondos_up_back.application.port.out.GerenteOutPort;
 import com.byma.fondos_up_back.application.validation.Validador;
 import com.byma.fondos_up_back.domain.model.Gerente;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class GerenteService implements GerenteInPort {
 
     private final GerenteOutPort gerenteOutPort;
@@ -19,23 +21,27 @@ public class GerenteService implements GerenteInPort {
 
     @Override
     public Gerente crear(Gerente gerenteRequestDTO) {
+        log.info("Crear gerente: {}", gerenteRequestDTO);
         Validador.validadorParametrosNull(gerenteRequestDTO);
         return gerenteOutPort.crear(gerenteRequestDTO);
     }
 
     @Override
     public List<Gerente> listarGerentes() {
+        log.info("Obtener todos los gerentes");
         return gerenteOutPort.listarGerentes();
     }
 
     @Override
     public Gerente obtenerPorIdOrganizacionGerente(Long idOrganizacionGerente) {
+        log.info("Obtener gerente por el id de organizacion: {}", idOrganizacionGerente);
         Validador.validadorParametrosNull(idOrganizacionGerente);
         return gerenteOutPort.obtenerPorIdOrganizacionGerente(idOrganizacionGerente);
     }
 
     @Override
     public Gerente actualizar(Long idOrganizacionGerente, Gerente gerenteRequestDTO) {
+        log.info("Actualizar gerente con id de organizacion: {}, datos a actualizar: {}",idOrganizacionGerente, gerenteRequestDTO);
         Validador.validadorParametrosNull(idOrganizacionGerente, gerenteRequestDTO);
         gerenteRequestDTO.setIdOrganizacionGerente(idOrganizacionGerente);
         return gerenteOutPort.actualizar(gerenteRequestDTO);
@@ -43,6 +49,7 @@ public class GerenteService implements GerenteInPort {
 
     @Override
     public void eliminar(Long idOrganizacionGerente) {
+        log.info("Eliminar gerente");
         Validador.validadorParametrosNull(idOrganizacionGerente);
         gerenteOutPort.eliminar(idOrganizacionGerente);
     }
--- src/main/java/com/byma/fondos_up_back/infrastructure/adapter/in/web/controller/GerenteController.java
+++ src/main/java/com/byma/fondos_up_back/infrastructure/adapter/in/web/controller/GerenteController.java
@@ -7,6 +7,7 @@ import com.byma.fondos_up_back.infrastructure.adapter.in.web.dto.request.Gerente
 import com.byma.fondos_up_back.infrastructure.adapter.in.web.dto.response.GerenteResponseDTO;
 import com.byma.fondos_up_back.infrastructure.adapter.in.web.mapper.GerenteControllerMapper;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/gerentes")
+@Slf4j
 public class GerenteController {
 
     private final GerenteInPort gerenteInPort;
@@ -31,35 +33,55 @@ public class GerenteController {
 
     @PostMapping("")
     public ResponseEntity<GerenteResponseDTO> crear(@RequestBody @Valid GerenteRequestDTO gerenteRequestDTO) {
+
+        log.info("Solicitud para crear un gerente: {}", gerenteRequestDTO);
         Validador.validadorParametrosNull(gerenteRequestDTO);
         Gerente gerenteCreado = gerenteInPort.crear(GerenteControllerMapper.gerenteRequestDtoAGerente(gerenteRequestDTO));
+        log.info("Creacion de gerente finalizada: {}", gerenteCreado);
+
         return ResponseEntity.ok().body(GerenteControllerMapper.gerenteAGerenteResponseDTO(gerenteCreado));
     }
 
     @GetMapping("")
     public ResponseEntity<List<GerenteResponseDTO>> listarGerentes() {
+
+        log.info("Solicitud para obtener todos los gerentes");
         List<Gerente> gerentes = gerenteInPort.listarGerentes();
+        log.info("Finalizacion de la solicitud para obtener todos los gerentes");
+
         return ResponseEntity.ok().body(gerentes.stream().map(GerenteControllerMapper::gerenteAGerenteResponseDTO).toList());
     }
 
     @GetMapping("/{idOrganizacionGerente}")
     public ResponseEntity<GerenteResponseDTO> obtenerPorIdOrganizacionGerente(@PathVariable Long idOrganizacionGerente) {
+
+        log.info("Solicitud para obtener gerente por id de organizacion: {}", idOrganizacionGerente);
         Validador.validadorParametrosNull(idOrganizacionGerente);
         Gerente gerente = gerenteInPort.obtenerPorIdOrganizacionGerente(idOrganizacionGerente);
+        log.info("Finalizacion de obtener gerente por id de organicacion: {}", gerente);
+
         return ResponseEntity.ok().body(GerenteControllerMapper.gerenteAGerenteResponseDTO(gerente));
     }
 
     @PutMapping("/{idOrganizacionGerente}")
     public ResponseEntity<GerenteResponseDTO> actualizar(@PathVariable Long idOrganizacionGerente, @RequestBody @Valid GerenteRequestDTO gerenteRequestDTO) {
+
+        log.info("Solicitud para actualizar gerente por id de organizacion: {}, datos a actualizar: {}",idOrganizacionGerente, gerenteRequestDTO);
         Validador.validadorParametrosNull(idOrganizacionGerente, gerenteRequestDTO);
         Gerente gerenteActualizado = gerenteInPort.actualizar(idOrganizacionGerente, GerenteControllerMapper.gerenteRequestDtoAGerente(gerenteRequestDTO));
+        log.info("Finalizacion de actualizacion de gerente, gerente actualizado: {}", gerenteActualizado);
+
         return ResponseEntity.ok().body(GerenteControllerMapper.gerenteAGerenteResponseDTO(gerenteActualizado));
     }
 
     @DeleteMapping("/{idOrganizacionGerente}")
     public ResponseEntity<Void> eliminar(@PathVariable Long idOrganizacionGerente) {
+
+        log.info("Solicitud para eliminar gerente con id de organizacion: {}", idOrganizacionGerente);
         Validador.validadorParametrosNull(idOrganizacionGerente);
         gerenteInPort.eliminar(idOrganizacionGerente);
+        log.info("Finalizacion de eliminacion de gerente con id de organizacion: {}", idOrganizacionGerente);
+
         return ResponseEntity.ok().build();
     }
 }
