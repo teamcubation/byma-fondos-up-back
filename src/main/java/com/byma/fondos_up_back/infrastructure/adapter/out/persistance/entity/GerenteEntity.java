d3b5c1bd7e2861a247afde9187851e68128702d3
maxizelazny <maximiliano.zelazny@tecval.com.ar>
2024-11-08 12:54:08 -0300

    comentarios dami

--- src/main/java/com/byma/fondos_up_back/infrastructure/adapter/out/persistance/entity/GerenteEntity.java
+++ src/main/java/com/byma/fondos_up_back/infrastructure/adapter/out/persistance/entity/GerenteEntity.java
@@ -1,12 +1,10 @@
 package com.byma.fondos_up_back.infrastructure.adapter.out.persistance.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity(name = "gerente")
+@Table(name = "gerentes")
 @Getter
 @Setter
 @AllArgsConstructor
